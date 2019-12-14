20f9186625aaeebe57cbb5378fee6b4a628b8f2b
TatsianaMark <tanya030890@mail.ru>
2019-12-14 15:54:27 +0300

    created navbar

---
+++ src/components/Details.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class Details extends Component {
+    render() {
+        return (
+            <div>
+             <h3>hi from details</h3>
+            </div>
+        );
+    }
+}
---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+export class Product extends Component {
+    render() {
+        return (
+            <div>
+               <h3>hi from pro</h3>
+            </div>
+        );
+    }
+}
