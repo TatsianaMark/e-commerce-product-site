8e52ce363a3440e23598b4e92cdc576a49b9cca3
TatsianaMark <tanya030890@mail.ru>
2019-12-15 22:55:59 +0300

    added Modal component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import { Switch, Route}  from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import logo from './logo.svg';
 import {Navbar} from './components/Navbar';
 import {ProductList} from "./components/ProductList";
 import {Details} from "./components/Details";
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,78 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+import {ProductConsumer} from "../Context";
+import{ ButtonContainer} from "./Button";
+import {Link} from "react-router-dom";
+
 
 export default class  Modal extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello from mofel</h1>
-            </div>
+            <ProductConsumer>
+                {(value) =>{
+                    const { modalOpen,closeOpen } = value;
+                    const { img, title, price } = value.modalProduct;
+
+                    if(!modalOpen){
+                        return null;
+                    } else{
+                        return(
+                        <ModalContainer>
+                          <div className="container">
+                            <div className="row">
+                                <div  id ="modal" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
+                                    <h5>item added to the cart</h5>
+                                </div>
+                            </div>
+                          </div>
+                        </ModalContainer>
+                        );
+                    }
+                }}
+            </ProductConsumer>
         );
     }
 }
+
+const ModalContainer = styled.div`
+position:fixed;
+top:0;
+left:0;
+right:0;
+bottom;
+background: rgba (0,0,0,0.3);
+
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
