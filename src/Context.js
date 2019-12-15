41674b3e93824dcc2734e095490918d7ab2ad06b
TatsianaMark <tanya030890@mail.ru>
2019-12-15 17:25:50 +0300

    added detail Link and obj-s about info product

--- src/Context.js
+++ src/Context.js
@@ -2,35 +2,63 @@ import React, {Component} from 'react';
 import {storeProducts,detailProduct} from "./data";
 
 
-const ProductContex = React.createContext();
+const ProductContext = React.createContext();
 //Provider
 //consumer
 
 class ProductProvider extends Component {
     state ={
-        products:storeProducts,
-        detailProduct:detailProduct
+        products: [],
+        detailProduct: detailProduct
+    };
+    componentDidMount() {
+        this.setProducts();
     }
+
+    setProducts =() =>{
+        let tempProducts =[];
+        storeProducts.forEach(item =>{
+            const singleItem ={...item};
+            tempProducts = [...tempProducts,singleItem];
+        });
+        this.setState(()=>{
+            return  {products : tempProducts };
+        });
+    };
     handleDetail = ()=>{
         console.log('hello from detail');
-    }
+    };
     addToCart = ()=>{
         console.log('hello from add to cart');
-    }
+    };
+// test state
+    // tester = () =>{
+    //     console.log('State products :',this.state.products[0].inCart);
+    //     console.log('State products :',storeProducts[0].inCart);
+    //
+    //     const tempProducts = [...this.state.products];
+    //     tempProducts[0].inCart =true
+    //     this.setState(()=>{
+    //         return{products: tempProducts}
+    //     },()=>{
+    //         console.log('State products :',this.state.products[0].inCart);
+    //         console.log('State products :',storeProducts[0].inCart);
+    //     })
+    // }
     render() {
         return (
-            <ProductContex.Provider value = {{
+            <ProductContext.Provider value = {{
                 ...this.state,
                 handleDetail: this.handleDetail,
                 addToCart: this.addToCart
             }}>
                 {this.props.children}
-            </ProductContex.Provider>
+            </ProductContext.Provider>
         );
     }
 }
 
 
-const ProductConsumer = ProductContex.Consumer;
+const ProductConsumer = ProductContext.Consumer;
 
 export{ProductProvider,ProductConsumer};
--- src/components/Product.js
+++ src/components/Product.js
@@ -4,18 +4,20 @@ import { Link } from 'react-router-dom';
 import { ProductConsumer } from "../Context";
 import {faCartPlus} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import PropTypes from 'prop-types';
 
 
 export class Product extends Component {
     render() {
         const{id, title, img, price, inCart} = this.props.product;
         return (
-            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3" >
                 <div className="card">
-                    <div className="img-container p-5" onClick={()=>console.log('u clicked me on the img cont')
+                    <div className="img-container p-5 " onClick={()=>console.log('u clicked me on the img cont')
                     }>
                         <Link to="/details">
-                            <img src={img} className="img-fluid max-width: 100% height: auto" alt="card-img-top "/>
+                            <img src={img} alt="product" className="card-img-top"/>
+                            {/*img-fluid max-width: 100% height: auto*/}
                         </Link>
                         <button
                             className="cart-btn"
@@ -47,6 +49,64 @@ export class Product extends Component {
     }
 }
 
-const ProductWrapper = styled.div`
+Product.propTypes = {
+    product: PropTypes.shape({
+        id:PropTypes.number,
+        img:PropTypes.string,
+        title:PropTypes.string,
+        price:PropTypes.number,
+        inCart:PropTypes.bool
+    }).isRequired
+};
 
-`
+const ProductWrapper = styled.div`
+.card{
+    border-color:transparent;
+    transition: all 1s linear;
+}
+.card-footer{
+    background: transparent;
+    border-top: transparent;
+    transition:all 1s linear;
+}
+&:hover{
+    .card{
+        border: 0.04rem solid rgba(0,0,0,0.2);
+        box-shadow: 2px 2px 5px 0px rgba (0,0,0,0.2);  
+        }
+    .card-footer {
+        background: rgba(247, 247 ,247);
+        }
+    }
+    .img-container{
+        position: relative;
+        overflow: hidden;
+    }
+    .card-img-top{
+        transition: all 1s linear;
+    }
+    .img-container: hover .card-img-top{
+        transform: scale(1.2);
+    }
+    .cart-btn{
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        padding: 0.2rem 0.4rem;
+        background: var(--lightGreen);
+        border: none;
+        color: var(--mainWhite);
+        font-size: 1.4rem;
+        border-radius: 0.5rem 0 0 0;
+        transform: translate(100%,100%);
+        transition:all 1s linear;
+   }
+   .img-container: hover .cart-btn {
+        transform: translate(0, 0);
+   } 
+   .cart-btn: hover{
+       color:var(--mainGreen);
+       cursor: pointer;
+       
+   }
+`;
