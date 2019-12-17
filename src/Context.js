15840625ac41af570a4da6f7cc1cd3ba938a1a74
TatsianaMark <tanya030890@mail.ru>
2019-12-17 23:26:58 +0300

    fixed some error with icon

--- src/Context.js
+++ src/Context.js
@@ -88,13 +88,13 @@ class ProductProvider extends Component {
 
     increment = id =>{
         let tempCart = [...this.state.cart];
-        const selectedProduct = tempCart.find(item=>item.id === id)
+        const selectedProduct = tempCart.find(item=>item.id === id);
 
         const index = tempCart.indexOf(selectedProduct);
         const product = tempCart[index];
 
         product.count = product.count + 1;
-        product.total = product .count * product.price;
+        product.total = product.count * product.price;
 
         this.setState(()=>{return{cart:[...tempCart]}},()=>{this.addTotals()});
 
